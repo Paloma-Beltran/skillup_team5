02aaaa0b6615b388fffb25f7a87f185fab18607d
Manuel <ferman5903@hotmail.com>
2023-03-03 19:36:35 -0600

    Logo provisional, boceto pagina inicio

--- src/pages/PaginaInicio.jsx
+++ src/pages/PaginaInicio.jsx
@@ -2,12 +2,26 @@ import { Link } from "react-router-dom";
 
 function PaginaInicio(){
     return(
-        <div className="contenedor">
-            <h1>Inicio</h1>
-            <p>Poner información de la pagina</p>
-            <Link to="/usuario/1">Ejemplo perfil usuario</Link><br />
-            <Link to="/empresa/2">Ejemplo perfil empresa</Link>
-        </div>
+        <>
+            <header className="header contenedor">
+                <img src="skillup-logo.png" className="header__img" alt="Logo SkillUp" />
+                <div>
+                    <h1 className="titulo">SkillUp</h1>
+                    <p className="header__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, perspiciatis obcaecati. Velit, autem? Ratione optio et commodi quibusdam voluptate veniam quidem? Blanditiis placeat hic unde expedita, nulla veritatis optio atque.</p>
+                    <p className="header__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum maxime veniam non eveniet, unde excepturi, mollitia rem nihil maiores provident repellat consequuntur placeat atque minus, tempora autem voluptate. Aliquid, eos?</p>
+                    <div className="header__botones">
+                        <Link to="/ofertas" class="boton">Ofertas</Link>
+                        <Link to="/cursos" class="boton">Cursos</Link>
+                    </div>
+                </div>
+            </header>
+            <div className="contenedor">
+                <p>Poner información de la pagina</p>
+                <p>Poner recomendaciones de ofertas y cursos</p>
+                <Link to="/usuario/1">Ejemplo perfil usuario</Link><br />
+                <Link to="/empresa/2">Ejemplo perfil empresa</Link>
+            </div>
+        </>
     )
 }
 
