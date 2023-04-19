4e372b6d58e85f2acad4f866df12f68962b9f112
Manuel <ferman5903@hotmail.com>
2023-04-18 19:08:31 -0600

    Correccion de error al ver perfil sin tener usuario

--- src/pages/PaginaEmpresa.jsx
+++ src/pages/PaginaEmpresa.jsx
@@ -95,7 +95,7 @@ function PaginaEmpresa(){
                         {datosUsuario.nombre}
                         {
                             verificada && (
-                                <svg xmlns="http://www.w3.org/2000/svg" class="usuario__verificacion icon icon-tabler icon-tabler-discount-check" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="#000" fill="#0af" stroke-linecap="round" stroke-linejoin="round">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="usuario__verificacion icon icon-tabler icon-tabler-discount-check" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="#000" fill="#0af" strokeLinecap="round" strokeLinejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                     <path d="M5 7.2a2.2 2.2 0 0 1 2.2 -2.2h1a2.2 2.2 0 0 0 1.55 -.64l.7 -.7a2.2 2.2 0 0 1 3.12 0l.7 .7c.412 .41 .97 .64 1.55 .64h1a2.2 2.2 0 0 1 2.2 2.2v1c0 .58 .23 1.138 .64 1.55l.7 .7a2.2 2.2 0 0 1 0 3.12l-.7 .7a2.2 2.2 0 0 0 -.64 1.55v1a2.2 2.2 0 0 1 -2.2 2.2h-1a2.2 2.2 0 0 0 -1.55 .64l-.7 .7a2.2 2.2 0 0 1 -3.12 0l-.7 -.7a2.2 2.2 0 0 0 -1.55 -.64h-1a2.2 2.2 0 0 1 -2.2 -2.2v-1a2.2 2.2 0 0 0 -.64 -1.55l-.7 -.7a2.2 2.2 0 0 1 0 -3.12l.7 -.7a2.2 2.2 0 0 0 .64 -1.55v-1"></path>
                                     <path d="M9 12l2 2l4 -4"></path>
@@ -105,7 +105,7 @@ function PaginaEmpresa(){
                     </h2>
                     {
                         //! Correo que solo puede ver el admin y el dueño del perfil
-                        (usuario.rol == "admin" || uid == usuario.id) && <p className="usuario__correo">{datosUsuario.correo}</p>
+                        usuario && (usuario.rol == "admin" || uid == usuario.id) && <p className="usuario__correo">{datosUsuario.correo}</p>
                     }
                     <p className="usuario__direccion">{datosUsuario.direccion}</p>
                 </div>
